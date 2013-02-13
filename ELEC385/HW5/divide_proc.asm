7c9b65b404bf8919c25cf4a448aaba4744c81f26
kshiga <khiga1@lion.lmu.edu>
2013-02-13 11:41:22 -0800

    Update ELEC385/HW5/divide_proc.asm
    fixed comments, spacing

--- ELEC385/HW5/divide_proc.asm
+++ ELEC385/HW5/divide_proc.asm
@@ -6,31 +6,31 @@
     ; r2: dividend/remainder
     ; r3: divisor
     ; r4: branch destination address
-    ; r5: pc/proc pointer
+    ; r5: proc pointer
     ; r6: branch condition/calculation of dividend - divisor
     ; r7: stored program counter (returned address from subroutine)
     ; 
     ; NOTE: To Run, XXXXXMEMORY ADDRESS HEREXXXXXX  must be replaced with a valid memory address.
-    ;       and     XXXXXCODE ADDRESS HEREXXXXXX    must be replaced with a valid address.
+    ;          and  XXXXXCODE ADDRESS HEREXXXXXX    must be replaced with a valid address.
     ;       ** DO NOT SET THE SAME VALUE TO BOTH XXXXXMEMORY ADDRESS HEREXXXXXX 
     ;                                        AND XXXXXCODE ADDRESS HEREXXXXXX.
     ;  
     ;
-    ; *lines that have been commented out are only used for testing purposes
+    ; *lines that have been commented out are only used for testing purposes.
 
                 .org  XXXXX MEMORY ADDRESS HEREXXXXXX  ;* REPLACE XXXXXMEMORY ADDRESS HEREXXXXXX 
-                                                       ;  with your target address
-    dividend:   .dc  45 
-    divisor:    .dc  5
+                                                       ;  with your target memory address
+    dividend:   .dc  DIVIDEND        ; * REPLACE with decimal value DIVIDEND
+    divisor:    .dc  DIVISOR         ; * REPLACE with decimal value DIVISOR
     quotient:   .dc  0
     remainder:  .dc  0
 
                 .org XXXXXCODE ADDRESS HEREXXXXXX      ;* REPLACE XXXXXCODE ADDRESS HEREXXXXXX 
-                                                       ;  with your target address
+                                                       ;  with your target address for the code
                 ldr  r2, dividend    ; r2: dividend
                 ldr  r3, divisor     ; r3: divisor
                 lar  r5, proc        ; r5: points to procedure
-                brl  r7, r5          ; r8 gets pc of procedure
+                brl  r7, r5          ; r7: gets pc of procedure
           
                 st r1, quotient      ; stores quotient
                 st r2, remainder     ; stores remainder
@@ -50,7 +50,9 @@
                 brpl   r4, r6        ; if (most-current) dividend - divisor is greater than or equal to zero, we loop.
                                      ; this means that there the dividend has at least one instance of the divisor in
                                      ; it, thus, we can run through the loop again without getting a negative remainder
-                                     ; and incrementing the quotient to greater than what it should be  
-                br r7
+                                     ; and incrementing the quotient to greater than what it should be.
+                                     ;
+                br r7                ; after the branch condition yields false, we load the address at r7 
+                                     ; into the program counter, which sould bring us back to the linked branch
 
           
