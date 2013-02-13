bbefd9726b0de690ca4b4a3519d5d3144a193764
kshiga <khiga1@lion.lmu.edu>
2013-02-13 11:08:53 -0800

    Update ELEC385/HW5/divide.asm

--- ELEC385/HW5/divide.asm
+++ ELEC385/HW5/divide.asm
@@ -17,23 +17,24 @@
           ;.dc  45    ; test
           ;.dc  5    ; test
 
-          .org  0xA000
-          la    r1, 0        ; r1: quotient
-          la    r5, XXXXXADDRESS HEREXXXXXX   ; r5: pointer data
-          ld    r2, 0(r5)     ; r2: dividend/remainder 
-          ld    r3, 4(r5)     ; r3: divisor
-          lar   r4, loop      ; r4: branch destination address
+           .org  0xA000
+           la    r1, 0        ; r1: quotient
+           la    r5, XXXXXADDRESS HEREXXXXXX   ; r5: pointer data
+           ld    r2, 0(r5)     ; r2: dividend/remainder 
+           ld    r3, 4(r5)     ; r3: divisor
+           lar   r4, loop      ; r4: branch destination address
 
     loop:  sub   r2, r2, r3    ; subtracts divisor from dividend and stores in r2
-          addi  r1, r1, 1     ; increments quotient
-          sub   r6, r2, r3    ; calculates (most-current) dividend - divisor
-          brpl   r4, r6       ; if (most-current) dividend - divisor is greater than or equal to zero, we loop.
-                            ; this means that there the dividend has at least one instance of the divisor in
-                            ; it, thus, we can run through the loop again without getting a negative remainder
-                            ; and incrementing the quotient to greater than what it should be 
-          st    r1, 8(r5)     ; stores quotient in F8
-          st    r2, 12(r5)    ; stores remainder in FC
+           addi  r1, r1, 1     ; increments quotient
+           sub   r6, r2, r3    ; calculates (most-current) dividend - divisor
+           brpl   r4, r6       ; if (most-current) dividend - divisor is greater than or equal to zero, we loop.
+                               ; this means that there the dividend has at least one instance of the divisor in
+                               ; it, thus, we can run through the loop again without getting a negative remainder
+                               ; and incrementing the quotient to greater than what it should be 
+           st    r1, 8(r5)     ; stores quotient in F8
+           st    r2, 12(r5)    ; stores remainder in FC
 
-          ;ldr    r20, 0xF8    ; test to see stored quotient
-          ;ldr    r21, 0xFC    ; test to see stored remainder
-          stop
+           ;ldr    r20, 0xF8    ; test to see stored quotient
+           ;ldr    r21, 0xFC    ; test to see stored remainder
+           
+           stop
